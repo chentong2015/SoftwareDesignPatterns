739f18a280cd866fefbc630b4ece5ab97646ea63
Tong Chen <tong.chen@etu.utc.fr>
2021-05-21 17:58:20 +0200

    add new leader-follower design patterns

--- src/clean_code/BaseCleanCode.java
+++
@@ -1,4 +0,0 @@
-package clean_code;
-
-public class BaseCleanCode {
-}
---
+++ src/core/BaseArchitect.java
@@ -0,0 +1,15 @@
+package core;
+
+public class BaseArchitect {
+
+    // 实战模式:
+    // Leader - Follower模式
+    // Producer - consumer模式
+    // Server - Client模式
+
+    // 4层分层架构:
+    // 1. 表示层
+    // 2. 应用层
+    // 3. 领域层(模型，服务)
+    // 4. 基础结构层(IoC, 缓存, 存储库)
+}
