fae18ffd174617c7ee75710e2b74ad3de3447b70
Tong Chen <tong.chen@etu.utc.fr>
2021-09-01 16:05:37 +0200

    add base dao and dto

--- src/design_patterns/ChainResponsibility/BaseChainResponsibility.java
+++ src/design_patterns/ChainResponsibility/BaseChainResponsibility.java
@@ -1,6 +1,84 @@
 package design_patterns.ChainResponsibility;
 
 // 责任链设计模式
+// 运用场景：MyBatis源码中关于Plugins插件的实现
+// public class Configuration 
+//    protected final InterceptorChain interceptorChain = new InterceptorChain();
 public class BaseChainResponsibility {
+	
+	// 
+	// public class InterceptorChain {
+    // 
+    //   private final List<Interceptor> interceptors = new ArrayList<>();
+    // 
+    //   public Object pluginAll(Object target) {
+    //     for (Interceptor interceptor : interceptors) {
+    //       target = interceptor.plugin(target);
+    //     }
+    //     return target;
+    //   }
+    // 
+    //   public void addInterceptor(Interceptor interceptor) {
+    //     interceptors.add(interceptor);
+    //   }
+    // 
+    //   public List<Interceptor> getInterceptors() {
+	//     // Returns an unmodifiable view of the specified list.
+    //     return Collections.unmodifiableList(interceptors);
+    //   }
+    // 
+    // }
+    // 
+	
+	// public interface Interceptor {
+    //
+    //   Object intercept(Invocation invocation) throws Throwable;
+    //   
+    //   default Object plugin(Object target) {
+    //     return Plugin.wrap(target, this);
+    //   }
+    //   
+    //   default void setProperties(Properties properties) {
+    //     // NOP
+    //   }
+    //
+    // }
 
+	// public class Plugin implements InvocationHandler {
+    //
+    // private final Object target;
+    // private final Interceptor interceptor;
+    // private final Map<Class<?>, Set<Method>> signatureMap;
+    //
+    // private Plugin(Object target, Interceptor interceptor, Map<Class<?>, Set<Method>> signatureMap) {
+    //   this.target = target;
+    //   this.interceptor = interceptor;
+    //   this.signatureMap = signatureMap;
+    // }
+    //
+    // public static Object wrap(Object target, Interceptor interceptor) {
+    //   Map<Class<?>, Set<Method>> signatureMap = getSignatureMap(interceptor);
+    //   Class<?> type = target.getClass();
+    //   Class<?>[] interfaces = getAllInterfaces(type, signatureMap);
+    //   if (interfaces.length > 0) {
+    //     return Proxy.newProxyInstance(
+    //         type.getClassLoader(),
+    //         interfaces,
+    //         new Plugin(target, interceptor, signatureMap));
+    //   }
+    //   return target;
+    // }
+    //
+    // @Override
+    // public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    //   try {
+    //     Set<Method> methods = signatureMap.get(method.getDeclaringClass());
+    //     if (methods != null && methods.contains(method)) {
+    //       return interceptor.intercept(new Invocation(target, method, args));
+    //     }
+    //     return method.invoke(target, args);
+    //   } catch (Exception e) {
+    //     throw ExceptionUtil.unwrapThrowable(e);
+    //   }
+    // }
 }
