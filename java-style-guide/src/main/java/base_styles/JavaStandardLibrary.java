6685e0d6dcffb082df58441d851c49ec3a12787e
tchen <tchen@murex.com>
2023-10-15 16:49:31 +0200

    use secure random to replace random class

--- java-style-guide/src/main/java/base_styles/JavaStandardLibrary.java
+++ java-style-guide/src/main/java/base_styles/JavaStandardLibrary.java
@@ -1,5 +1,7 @@
 package base_styles;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Random;
 import java.util.SplittableRandom;
 import java.util.concurrent.ThreadLocalRandom;
@@ -13,11 +15,18 @@ public class JavaStandardLibrary {
     // - Random使用线程安全的伪随机数生成器，造成过度同步  => 将每个操作都进行同步
     // - ThreadLocalRandom能够产出高质量的随机数，高性能 => 只用于单线程
     // - SplittableRandom并行一共随机数Stream，速度快
-    public void getRandomNumber(int n) {
+    public void getRandomNumber(int n) throws NoSuchAlgorithmException {
         int value1 = new Random().nextInt(n);
+
         int value2 = ThreadLocalRandom.current().nextInt(n);
 
         SplittableRandom random = new SplittableRandom();
         int value3 = random.nextInt();
+
+        // SecureRandom is preferred to Random 企业中推荐使用SecureRandom
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.nextInt();
+        Random rand = SecureRandom.getInstanceStrong();
+        int value4 = rand.nextInt();
     }
 }
