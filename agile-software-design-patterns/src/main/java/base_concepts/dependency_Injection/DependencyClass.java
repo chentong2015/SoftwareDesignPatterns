17c83681b2449029a8b76c8906bb36933caba621
chen tong <chen2015tong@gmail.com>
2023-12-27 17:54:57 +0100

    delete software design basic concepts

--- agile-software-design-patterns/src/main/java/base_concepts/dependency_Injection/DependencyClass.java
+++
@@ -1,9 +0,0 @@
-package base_concepts.dependency_Injection;
-
-public class DependencyClass implements IDependencyClass {
-
-    @Override
-    public void doSomething() {
-        System.out.println("do something ...");
-    }
-}
--- agile-software-design-patterns/src/main/java/base_concepts/dependency_Injection/IDependencyClass.java
+++
@@ -1,6 +0,0 @@
-package base_concepts.dependency_Injection;
-
-public interface IDependencyClass {
-
-    void doSomething();
-}
