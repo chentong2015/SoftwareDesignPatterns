f937c11e30b8db170b74745b6a7acba338d647c2
chen tong <chen2015tong@gmail.com>
2024-03-03 14:50:41 +0100

    update immutatble calsss

--- refactoring-guide/src/main/java/immutable/ImmutableClassQuestion.java
+++ refactoring-guide/src/main/java/immutable/ImmutableClassQuestion.java
@@ -5,13 +5,14 @@ import java.util.Date;
 // 将以下的类型设计成不可变类型，类型的实例在被创建后不可变化
 // 1. init 在初始化对象属性值时创建另一个对象
 // 2. getter 返回具有相同属性值的另一个对象
-public class MyImmutableClassDemo {
+public class ImmutableClassQuestion {
 
-    // TODO. 注意String是否会给不可变性造成影响
+    // TODO. String作为不可变类型，本身不会影响类型的不可变性
+    // String变量每次修改或赋值时都会重新创建新的对象，并将新对象的引用赋值给变量
     private String name;
     private Date dateTime;
 
-    public MyImmutableClassDemo(String name, Date dateTime) {
+    public ImmutableClassQuestion(String name, Date dateTime) {
         this.name = name;
         this.dateTime = dateTime;
         // this.dateTime = new Date(dateTime.getTime());
@@ -27,18 +28,14 @@ public class MyImmutableClassDemo {
     }
 
     public static void main(String[] args) {
-        MyImmutableClassDemo demo = new MyImmutableClassDemo("name", new Date());
-
-        // TODO. 通过外部的String对象修改不了demo对象的属性值
-        //  String作为不可变类型，每次赋值时都会重新创建新的对象，并将对象的引用赋值给遍历
+        ImmutableClassQuestion demo = new ImmutableClassQuestion("name", new Date());
         String name = demo.getName();
         name = "new name";
+        System.out.println(demo.getName());
 
-        // TODO. 修改取出的属性对象，才能造成对象本身被修改
+        // 在外部修改对象的"引用类型的属性"时会间接修改原对象
         Date date = demo.getDateTime();
         date.setTime(100l);
-
-        System.out.println(demo.getName());
         System.out.println(demo.getDateTime().getTime());
     }
 }
